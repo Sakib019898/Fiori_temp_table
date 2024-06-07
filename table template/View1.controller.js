f0a669c850e34833c6e276e29791821bf5c00452
Sakib Ahmed <41849444+Sakib019898@users.noreply.github.com>
2024-06-07 17:49:34 +0600

    Add files via upload

---
+++ table template/View1.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("odatacrud.controller.View1", {
+        onInit: function () {
+           this.onReadAll();
+        },
+        onReadAll: function(){
+            var that = this;
+            var oModel = this.getOwnerComponent().getModel();
+            oModel.read("/Products",{
+                success: function(odata){
+                        var jModel = new sap.ui.model.json.JSONModel(odata);
+                        that.getView().byId("idProducts").setModel(jModel);
+                }, error:function(oError){
+                    console.log(oError);
+                }
+            });
+
+        }
+    });
+});
